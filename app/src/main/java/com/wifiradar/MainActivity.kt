11acd550a977af235e6c016672ea6fb36cc653df
Roman <romanshutyak@gmail.com>
2026-06-27 16:49:00 +0300

    ерший коміт: структура проекту

---
+++ app/src/main/java/com/wifiradar/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.wifiradar
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Поки що екран порожній. 
+        // Саме сюди ми згодом додамо логіку 1-хвилинного сканування та виклик ARCore.
+    }
+}
